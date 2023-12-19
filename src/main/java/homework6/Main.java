d46c55c3e9f549e1634380cbec63a792e4e803ec
Michanco Slesarev <michanco80@gmail.com>
2023-12-20 01:30:48 +0300

    Homework6 ready

---
+++ src/main/java/homework6/Main.java
@@ -0,0 +1,26 @@
+package homework6;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        System.out.println(binarySearch(array, 15));
+    }
+
+    public static int binarySearch(int[] array, int searchNumber) {
+        int leftIndex = 0;
+        int rightIndex = array.length - 1;
+        int halfIndex = array.length / 2;
+        while (rightIndex != leftIndex) {
+            if (searchNumber == array[halfIndex]) {
+                return halfIndex;
+            } else if (searchNumber > array[halfIndex]) {
+                leftIndex = halfIndex;
+                halfIndex = leftIndex + (rightIndex - leftIndex ) / 2 + 1;
+            } else {
+                rightIndex = halfIndex;
+                halfIndex = leftIndex + (rightIndex - leftIndex) / 2;
+            }
+        }
+        return -1;
+    }
+}
